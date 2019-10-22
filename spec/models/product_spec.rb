c5e1f28629fe7fc76223da3f5fa391b82e58973a
Kaleb Haslam <kalebmarc@hotmail.com>
2019-10-22 20:36:20 +0000

    use subject in product_spec test to DRY code up (still not working as intended though

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,40 +3,34 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   @category = Category.new(name: 'test')
 
+  subject {
+    described_class.new(name: 'name', description: 'test', image: 'test', price_cents: 100, quantity: 100, category: @category)
+  }
+
   describe 'Validations' do
 
-    xit 'validates when all values are correct' do
-      @product  = Product.new(name: 'name', description: 'test', image: 'test', price_cents: 100, quantity: 100, category: @category)
-      
-      expect(@product).to be_valid
+    xit 'validates when all values are correct' do    
+      expect(subject).to be_valid
     end
 
     it 'validates name' do
-      @product  = Product.new(description: 'test', image: 'test', price_cents: 100, quantity: 100, category: @category)
-      
-      expect(@product.name).to be_nil
-      expect(@product).not_to be_valid
+      subject.name = nil
+      expect(subject).not_to be_valid
     end
 
     it 'validates price' do
-      @product  = Product.new(name: 'test', description: 'test', image: 'test', quantity: 100, category: @category)
-      
-      expect(@product.price_cents).to be_nil
-      expect(@product).not_to be_valid
+      subject.price = nil
+      expect(subject).not_to be_valid
     end
 
     it 'validates quantity' do
-      @product  = Product.new(name: 'test', description: 'test', image: 'test', price_cents: 100, category: @category)
-      
-      expect(@product.quantity).to be_nil
-      expect(@product).not_to be_valid
+      subject.quantity = nil
+      expect(subject).not_to be_valid
     end
 
     it 'validates category' do
-      @product  = Product.new(name: 'test', description: 'test', image: 'test', price_cents: 100, quantity: 100)
-      
-      expect(@product.category).to be_nil
-      expect(@product).not_to be_valid
+      subject.category = nil
+      expect(subject).not_to be_valid
     end
   end
 end
