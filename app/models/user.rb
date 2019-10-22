1d62a6ded3fd73e2bbd22c12eaae4715c3b19687
Kaleb Haslam <kalebmarc@hotmail.com>
2019-10-22 20:03:37 +0000

    move user authentication to User class method & wrote tests for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,4 +52,24 @@ RSpec.describe User, type: :model do
     end
 
   end
+
+  describe '.authenticate_with_credentials' do
+    
+    it 'authenticate a user with correct credentials' do
+      @user = User.new(first_name: 'Firstname', last_name: 'Lastname', email: 'testuser@example.com', password: 'Hunter2', password_confirmation: 'Hunter2')
+      @user.save
+
+      user = User.authenticate_with_credentials('testuser@example.com', 'Hunter2')
+      expect(user).not_to be_nil
+    end
+
+    it 'not authenticate a user with wrong credentials' do
+      @user = User.new(first_name: 'Firstname', last_name: 'Lastname', email: 'testuser@example.com', password: 'Hunter2', password_confirmation: 'Hunter2')
+      @user.save
+
+      user = User.authenticate_with_credentials('testuser@example.com', 'NotHunter2')
+      expect(user).to be_nil
+    end
+
+  end
 end
