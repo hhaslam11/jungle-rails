c6e132bdc89ce4b4ee12d3700442cb79715c1756
Kaleb Haslam <kalebmarc@hotmail.com>
2019-10-22 05:43:57 +0000

    add field validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it 'validates with all fields correct' do
+      @user = User.new(first_name: 'Firstname', last_name: 'Lastname', email: 'testuser@example.com', password: 'Hunter2', password_confirmation: 'Hunter2')
+      expect(@user).to be_valid
+    end
+
+    it 'validates first_name field' do
+      @user = User.new(last_name: 'Lastname', email: 'testuser@example.com', password: 'Hunter2', password_confirmation: 'Hunter2')
+      expect(@user).not_to be_valid
+    end
+
+    it 'validates last_name field' do
+      @user = User.new(first_name: 'Firstname', email: 'testuser@example.com', password: 'Hunter2', password_confirmation: 'Hunter2')
+      expect(@user).not_to be_valid
+    end
+
+    it 'validates email field' do
+      @user = User.new(first_name: 'Firstname', last_name: 'Lastname', password: 'Hunter2', password_confirmation: 'Hunter2')
+      expect(@user).not_to be_valid
+    end
+
+    it 'validates password field' do
+      @user = User.new(first_name: 'Firstname', last_name: 'Lastname', email: 'testuser@example.com', password_confirmation: 'Hunter2')
+      expect(@user).not_to be_valid
+    end
+
+    it 'validates password_confirmation field' do
+      @user = User.new(first_name: 'Firstname', last_name: 'Lastname', email: 'testuser@example.com', password: 'Hunter2')
+      expect(@user).not_to be_valid
+    end
+    
+  end
 end
