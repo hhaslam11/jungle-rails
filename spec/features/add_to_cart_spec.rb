7e87377d8d8520efc941be56ce63dfb6efe63042
Kaleb Haslam <kalebmarc@hotmail.com>
2019-10-23 01:05:06 +0000

    add spec file to test adding product to cart

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.feature "AddToCarts", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They add a product to their cart" do
+    visit root_path
+
+    first(:link_or_button, 'Add').click
+    expect(page).to have_content('My Cart (1)')
+  end
 end
