aff51a5cbb3ed707b4266a02e3a33afd4a65ba33
Kaleb Haslam <kalebmarc@hotmail.com>
2019-10-23 00:39:51 +0000

    test for clicking on details paage

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They can click on the details button to see more details" do
+    visit root_path
+
+    first(:link, 'Details').click
+    expect(page).to have_css('.products-show')
+  end
 end
